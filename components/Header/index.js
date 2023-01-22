5cf0cdc4e77b522a969f3c5c8cfeecbb6df8ec83
Julia Löschel <juloeschel@gmail.com>
2023-01-22 19:44:01 +0100

    implement functionality

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import CustomProgress from "../CustomProgress";
 
@@ -32,21 +33,88 @@ const StyledText = styled.p`
   color: white;
 `;
 
-export default function Header({ sessionCountGlobal, checkedCountGlobal }) {
+const Button = styled.button`
+  position: absolute;
+  right: 2rem;
+  top: 3rem;
+  padding: 3rem 1rem;
+  border: none;
+  background-color: var(--neon);
+  border-radius: 1rem;
+`;
+
+const ButtonClose = styled.button`
+  position: absolute;
+  right: 2rem;
+  bottom: 2rem;
+  padding: 3rem 1rem;
+  border: none;
+  background-color: var(--neon);
+  border-radius: 1rem;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  position: absolute;
+  top: 100px;
+  min-height: 400px;
+  max-height: 600px;
+  width: 90%;
+  background-color: white;
+  padding: 0.5rem;
+  margin: 1rem;
+  border-radius: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  z-index: 1;
+`;
+
+export default function Header({
+  sessionCountGlobal,
+  checkedCountGlobal,
+  uncheckedGlobal,
+}) {
+  const [seeUnchecked, setSeeUnchecked] = useState(false);
+
+  const topics = Object.keys(uncheckedGlobal);
+
   return (
-    <StyledHeader>
-      <div>
-        <StyledHeadline>SuperCoach</StyledHeadline>
-        <StyledSubline>Track your achievements!</StyledSubline>
-      </div>
-      <div>
-        <StyledText>Your overall progress</StyledText>
-        <CustomProgress
-          width="100%"
-          max={sessionCountGlobal}
-          value={checkedCountGlobal}
-        />
-      </div>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <Button onClick={() => setSeeUnchecked(!seeUnchecked)}>chances</Button>
+        <div>
+          <StyledHeadline>SuperCoach</StyledHeadline>
+          <StyledSubline>Track your achievements!</StyledSubline>
+        </div>
+        <div>
+          <StyledText>Your overall progress</StyledText>
+          <CustomProgress
+            width="100%"
+            max={sessionCountGlobal}
+            value={checkedCountGlobal}
+          />
+        </div>
+      </StyledHeader>
+      {seeUnchecked && (
+        <Section>
+          <h3>that are your chances</h3>
+          {topics.map((topic) => {
+            return (
+              <ul>
+                {uncheckedGlobal[topic].map((session) => (
+                  <li>{session.name}</li>
+                ))}
+              </ul>
+            );
+          })}
+          <ButtonClose onClick={() => setSeeUnchecked(false)}>
+            CLOSE
+          </ButtonClose>
+        </Section>
+      )}
+    </>
   );
 }
